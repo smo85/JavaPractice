328c1da9eea039f9360a116e13a32a4788b9798a
Sarah Morris <sarah.morris@geappliances.com>
2025-06-26 14:00:41 -0400

    hashCode, more efficient way to find if list contains

--- Basics.java
+++ Basics.java
@@ -1,28 +1,49 @@
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Basics {
     public static void main(String[] args) {
-        varargsTest( "name", "is", "sarah");
 
-//        firstOfFirst(Arrays.asList("one", "two"), Collections.emptyList());
-
-
-    }
-
-    private static void varargsTest(String...values) {
-        for(String i : values) {
-            System.out.println(i);
+        // inefficient
+//        List<String> words = Arrays.asList("one", "seven", "eleven");
+//        if(words.contains("seven")) {
+//            System.out.println("found the word!");
+//        }
+        Map<User, User> users = new HashMap<>();
+        User user1 = new User(1L, "John", "john@domain.com");
+        User user2 = new User(2L, "Jennifer", "jennifer@domain.com");
+        User user3 = new User(3L, "Mary", "mary@domain.com");
+        users.put(user1, user1);
+        users.put(user2, user2);
+        users.put(user3, user3);
+        if (users.containsKey(user1)) {
+            System.out.print("User found in the collection");
         }
+
+
     }
 
-    static String firstOfFirst(List<String>... strings) {
-        List<Integer> ints = Collections.singletonList(42);
-        Object[] objects = strings;
-        objects[0] = ints; // Heap pollution
+    public static class User {
+        long id;
+        String name;
+        String email;
 
-        return strings[0].get(0); // ClassCastException
+        public User(long id, String name, String email) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+
+        public int hashCode() {
+            int hash = 7;
+            hash = 31 * hash + (int) id;
+            hash = 31 * hash + (name == null ? 0 : name.hashCode());
+            hash = 31 * hash + (email == null ? 0 : email.hashCode());
+            System.out.println("hashCode() called - Computed hash: " + hash);
+            return hash;
+        }
     }
 }
 
