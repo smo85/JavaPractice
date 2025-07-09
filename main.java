46f9270db2d51e692cd705be3f94338dbf7dc4ef
Sarah Morris <sarah.morris@geappliances.com>
2025-07-09 17:02:38 -0400

    abstract classes

--- main.java
+++ main.java
@@ -1,16 +1,7 @@
 public class main {
   public static void main(String[] args) {
-    // the new keyword is responsible for allocating memory for the new
-    // object through a constructor
-    // the constructor is used to initialize variables representing the main
-    // properties of the created object
-    HealthData myData = new HealthData(31, 65, "Sarah");
-    HealthData myData2 = new HealthData(myData, "Devin");
-    System.out.println(myData.toString());
-    myData2.setHrv(40);
-    System.out.println(myData2.toString());
-
-    HealthData nullData = new HealthData();
-    assert nullData.getUserName()  == null;
+    Enemy dracula = new Enemy(0, 0, "Dracula","vampire", 50);
+    dracula.move();
+    System.out.println(dracula);
   }
 }
